b7babdbe341bf8ec173dffad51bf31300ad8104a
Octron2008 <129083398+Octron2008@users.noreply.github.com>
2023-04-20 18:02:11 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
-set_dist = 0
-state = 0
-rawdist = 0
-dist = 0
-time_start = 0
-time_finish = 0
-time1 = 0
-time2 = 0
-sum_time = 0
-total_distance = 0
-
 def on_button_pressed_a():
-    global set_dist, state
-    set_dist = 10
-    pins.digital_write_pin(DigitalPin.P15, 1)
+    global state
     state = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    basic.show_icon(IconNames.SAD)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+total_distance = 0
+sum_time = 0
+time2 = 0
+time1 = 0
+time_finish = 0
+time_start = 0
+dist = 0
+rawdist = 0
+state = 0
+state = 10
+set_dist = 10
+pins.digital_write_pin(DigitalPin.P15, 1)
+
 def on_forever():
     global rawdist, dist, state, time_start, time_finish, time1, time2, sum_time, total_distance
-    while state == 0:
-        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-            kitronik_motor_driver.MotorDirection.FORWARD,
-            50)
-        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-            kitronik_motor_driver.MotorDirection.FORWARD,
-            50)
+    while True:
         pins.digital_write_pin(DigitalPin.P15, 0)
         pins.digital_write_pin(DigitalPin.P15, 1)
         basic.pause(200)
         rawdist = pins.analog_read_pin(AnalogPin.P2)
         dist = rawdist * 1.1
+        basic.show_number(dist)
         basic.pause(200)
+    while state == 0:
+        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
+            kitronik_motor_driver.MotorDirection.REVERSE,
+            50)
+        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
+            kitronik_motor_driver.MotorDirection.REVERSE,
+            50)
         if dist < set_dist:
             state = 1
     while state == 1:
+        basic.pause(500)
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
             kitronik_motor_driver.MotorDirection.FORWARD,
             75)
@@ -43,16 +50,17 @@ def on_forever():
         basic.pause(750)
         time_start = control.millis()
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-            kitronik_motor_driver.MotorDirection.FORWARD,
+            kitronik_motor_driver.MotorDirection.REVERSE,
             50)
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-            kitronik_motor_driver.MotorDirection.FORWARD,
+            kitronik_motor_driver.MotorDirection.REVERSE,
             50)
         if dist < set_dist:
             time_finish = control.millis()
             time1 = time_finish - time_start
             state = 2
     while state == 2:
+        basic.pause(500)
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
             kitronik_motor_driver.MotorDirection.FORWARD,
             50)
@@ -62,21 +70,15 @@ def on_forever():
         basic.pause(300)
         while dist < set_dist:
             kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-                kitronik_motor_driver.MotorDirection.FORWARD,
+                kitronik_motor_driver.MotorDirection.REVERSE,
                 50)
             kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-                kitronik_motor_driver.MotorDirection.FORWARD,
+                kitronik_motor_driver.MotorDirection.REVERSE,
                 50)
-            pins.digital_write_pin(DigitalPin.P15, 0)
-            pins.digital_write_pin(DigitalPin.P15, 1)
-            basic.pause(200)
-            rawdist = pins.analog_read_pin(AnalogPin.P2)
-            dist = rawdist * 1.1
-            basic.show_number(dist)
-            basic.pause(200)
         if dist < set_dist:
             state = 3
     while state == 3:
+        basic.pause(500)
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
             kitronik_motor_driver.MotorDirection.FORWARD,
             75)
@@ -86,23 +88,24 @@ def on_forever():
         basic.pause(750)
         time_start = control.millis()
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-            kitronik_motor_driver.MotorDirection.FORWARD,
+            kitronik_motor_driver.MotorDirection.REVERSE,
             50)
         kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-            kitronik_motor_driver.MotorDirection.FORWARD,
+            kitronik_motor_driver.MotorDirection.REVERSE,
             50)
         if dist < set_dist:
             time_finish = control.millis()
             time2 = time_finish - time_start
             kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-                kitronik_motor_driver.MotorDirection.FORWARD,
+                kitronik_motor_driver.MotorDirection.REVERSE,
                 0)
             kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-                kitronik_motor_driver.MotorDirection.FORWARD,
+                kitronik_motor_driver.MotorDirection.REVERSE,
                 0)
             state = 4
     while state == 4:
         sum_time = time1 + time2
         total_distance = sum_time * 22
+        total_distance = total_distance + 40
         basic.show_number(total_distance)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P15, 1)
     state = 0
+    test_state = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
 })
 let total_distance = 0
 let sum_time = 0
@@ -10,35 +13,49 @@ let time_finish = 0
 let time_start = 0
 let dist = 0
 let rawdist = 0
+let test_state = 0
 let state = 0
 state = 10
 let set_dist = 10
+test_state = 10
+pins.digitalWritePin(DigitalPin.P15, 1)
 basic.forever(function () {
     while (state == 0) {
-        while (true) {
-            pins.digitalWritePin(DigitalPin.P15, 0)
-            pins.digitalWritePin(DigitalPin.P15, 1)
-            basic.pause(200)
-            rawdist = pins.analogReadPin(AnalogPin.P2)
-            dist = rawdist * 1.1
-            basic.showNumber(dist)
-            basic.pause(200)
-        }
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(200)
+        rawdist = pins.analogReadPin(AnalogPin.P2)
+        dist = rawdist * 1.1
+        basic.showNumber(dist)
+        basic.pause(200)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
         if (dist < set_dist) {
             state = 1
         }
+    }
+    while (state == 0) {
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(200)
+        rawdist = pins.analogReadPin(AnalogPin.P2)
+        dist = rawdist * 1.1
+        basic.showNumber(dist)
+        basic.pause(200)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
+        if (dist < set_dist) {
+            state = 1
+        }
     }
     while (state == 1) {
-        while (true) {
-            pins.digitalWritePin(DigitalPin.P15, 0)
-            pins.digitalWritePin(DigitalPin.P15, 1)
-            rawdist = pins.analogReadPin(AnalogPin.P2)
-            dist = rawdist * 1.1
-            basic.showNumber(dist)
-        }
-        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(200)
+        rawdist = pins.analogReadPin(AnalogPin.P2)
+        dist = rawdist * 1.1
+        basic.showNumber(dist)
+        basic.pause(200)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 75)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 75)
         basic.pause(750)
@@ -52,49 +69,45 @@ basic.forever(function () {
         }
     }
     while (state == 2) {
-        while (true) {
-            pins.digitalWritePin(DigitalPin.P15, 0)
-            pins.digitalWritePin(DigitalPin.P15, 1)
-            basic.pause(200)
-            rawdist = pins.analogReadPin(AnalogPin.P2)
-            dist = rawdist * 1.1
-            basic.showNumber(dist)
-            basic.pause(200)
-        }
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(200)
+        rawdist = pins.analogReadPin(AnalogPin.P2)
+        dist = rawdist * 1.1
+        basic.showNumber(dist)
+        basic.pause(200)
         basic.pause(500)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
         basic.pause(300)
         while (dist < set_dist) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 50)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
         }
         if (dist < set_dist) {
             state = 3
         }
     }
     while (state == 3) {
-        while (true) {
-            pins.digitalWritePin(DigitalPin.P15, 0)
-            pins.digitalWritePin(DigitalPin.P15, 1)
-            basic.pause(200)
-            rawdist = pins.analogReadPin(AnalogPin.P2)
-            dist = rawdist * 1.1
-            basic.showNumber(dist)
-            basic.pause(200)
-        }
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(200)
+        rawdist = pins.analogReadPin(AnalogPin.P2)
+        dist = rawdist * 1.1
+        basic.showNumber(dist)
+        basic.pause(200)
         basic.pause(500)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 75)
         kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 75)
         basic.pause(750)
         time_start = control.millis()
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 50)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
         if (dist < set_dist) {
             time_finish = control.millis()
             time2 = time_finish - time_start
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 0)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 0)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 10)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 10)
             state = 4
         }
     }
